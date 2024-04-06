cc7e83f7bef5c7ae74814d1fb0ca97a9729ecaf5
Ashton Sometimes <155324426+AshtonSometimes@users.noreply.github.com>
2024-04-06 14:28:32 -0400

    Update home.js

--- public/src/home.js
+++ public/src/home.js
@@ -1,13 +1,17 @@
 function getTotalBooksCount(books) {
-  // YOUR SOLUTION HERE
+  return books.length;
 }
 
 function getTotalAccountsCount(accounts) {
-  // YOUR SOLUTION HERE
+  return accounts.length;
 }
 
 function getBooksBorrowedCount(books) {
-  // YOUR SOLUTION HERE
+  let booksCheckedOut = books.filter(
+    (book) =>
+     book.borrows.filter((record) => record.returned === false).length > 0
+   );
+   return booksCheckedOut.length;
   // Hint: You can use the [`filter()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter) method here. 
   // If you get stuck, feel free to take a look at this repl.it: https://replit.com/@thinkful/getBooksBorrowedCount#index.js
 }
